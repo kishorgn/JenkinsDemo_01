c278472636d48fed6a7e0c597184fab875bd85cc
Kishor <kishorgn@gmail.com>
2022-04-13 10:54:44 +0530

    Added a simple for loop

--- src/main/java/com/itc/App.java
+++ src/main/java/com/itc/App.java
@@ -9,6 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for(int c = 0 ; c < 10 ; c++) {
+        	System.out.println(c);
+        }
         System.out.println("Thank you!!!");
     }
 }
