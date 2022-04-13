3471efa8b727de05b040881858861a29fc1b0462
Kishor <kishorgn@gmail.com>
2022-04-13 10:59:41 +0530

    Added simple JUnit TestCase for the Calculator component.

---
+++ src/test/java/com/itc/beans/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.itc.beans;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	Calculator calc = null;
+	
+	@Before
+	public void init() {
+		calc = new Calculator();
+	}
+	@After
+	public void cleanup() {
+		calc = null;
+	}
+
+	@Test
+	public void testAdd() {
+		int expected = 13;
+		int actual = calc.add(8, 5);
+		assertEquals(expected, actual);
+	}
+
+}
